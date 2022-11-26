e0df5e4ac56371ed91ff108aba58e7a02e33293a
Maximillian Heth <maxxheth@protonmail.com>
2022-11-26 12:34:10 -0600

    added function declarations

---
+++ second-randomizer.d.ts
@@ -0,0 +1,3 @@
+export declare const secondRandomizer: (second: number) => number;
+export declare const createRandomizedSeconds: (qty: number) => number[];
+export declare const getRandomizedSecond: (qty: number) => number;
